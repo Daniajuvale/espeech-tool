aac9f2e500153a7244665d93c7f359ec24e19fbd
Dania Juvale <71597487+Daniajuvale@users.noreply.github.com>
2022-04-29 10:12:46 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ async function handleSuccess(stream) {
       // cv.imshow('canvasOutput', dst);
       socket.emit('image', data);
       console.log("Image sent")
-  }, 300);
+  }, 150);
 }
 
 function handleError(error) {
